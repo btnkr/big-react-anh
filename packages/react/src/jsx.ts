f16a67a2f41910bc0e9caab92ba0247279c60190
anhao <945237279@qq.com>
2024-07-08 16:37:47 +0800

    feat: 单测

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -61,6 +61,14 @@ export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
 	return ReactElement(type, key, ref, props);
 };
 
+export function isValidElement(object: any) {
+	return (
+		typeof object === 'object' &&
+		object !== null &&
+		object.$$typeof === REACT_ELEMENT_TYPE
+	);
+}
+
 export const jsxDEV = (type: ElementType, config: any) => {
 	let key: Key = null;
 	const props: Props = {};
--- packages/shared/ReactSymbols.ts
+++ packages/shared/ReactSymbols.ts
@@ -2,4 +2,4 @@ const supportSymbol = typeof Symbol === 'function' && Symbol.for;
 
 export const REACT_ELEMENT_TYPE = supportSymbol
 	? Symbol.for('react.element')
-	: 0xefcf;
+	: 0xeac7;
